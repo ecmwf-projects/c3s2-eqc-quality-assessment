a67406e5dd78635f6232aa86c1b11a0526f87c03
Olivier Burggraaff <olivierburggraaff@gmail.com>
2026-07-24 10:56:52 +0100

    validate-urls: Avoid duplicate checks, Avoid early crashes, Cleaner output (#639)
    * Refactor single URL check
    
    * Rewrite main: Find all URLs, check in order, return list of errors

--- scripts/validate-urls.py
+++ scripts/validate-urls.py
@@ -29,6 +29,7 @@ KNOWN_403_ISSUES = (
     "https://web.unisa.it",
 )
 
+ARROW = "    ->"
 CROSSREF_URL = "https://api.crossref.org/works/"
 URL_PATTERN = r"https?://(?:[^\s()]+|\([^\s()]*\))+"  # Allow pairs of brackets in link
 
@@ -46,73 +47,112 @@ def find_urls(notebook: nbformat.NotebookNode) -> list[str]:
     return sorted(unique_urls)
 
 
-def validate_urls(path: Path, *, verbose: bool = False) -> None:
-    notebook = nbformat.read(path, nbformat.NO_CONVERT)
+def validate_url(url: str, *, verbose: bool = False) -> Exception | None:
+    """Validate a single URL, checking for HTTP status and SSL errors."""
+    result: Exception | requests.exceptions.SSLError | None
+    if verbose:
+        url_orig = url  # Copy so can be reused later
+        print(url)
 
-    exceptions: dict[str, Exception] = {}
-    for url in find_urls(notebook):
+    url = url.replace("https://doi.org/", CROSSREF_URL)
+    if verbose:
+        if url != url_orig:  # If url has been changed
+            print(ARROW, url)
+
+    try:
+        response = requests.head(url, allow_redirects=True)
+        match response.status_code:
+            case 403:
+                response = requests.head(
+                    url,
+                    allow_redirects=True,
+                    headers={"User-Agent": USER_AGENT},
+                )
+            case 404 | 405:
+                if url.startswith(CROSSREF_URL):
+                    url = url.rstrip("/") + "/agency"
+                response = requests.get(url, allow_redirects=True)
+
         if verbose:
-            url_orig = url  # Copy so can be reused later
-            print(url)
+            print(ARROW, response.status_code)
 
-        url = url.replace("https://doi.org/", CROSSREF_URL)
-        if verbose:
-            if url != url_orig:  # If url has been changed
-                print("    ->", url)
-
-        try:
-            response = requests.head(url, allow_redirects=True)
-            match response.status_code:
-                case 403:
-                    response = requests.head(
-                        url,
-                        allow_redirects=True,
-                        headers={"User-Agent": USER_AGENT},
-                    )
-                case 404 | 405:
-                    if url.startswith(CROSSREF_URL):
-                        url = url.rstrip("/") + "/agency"
-                    response = requests.get(url, allow_redirects=True)
-
+        if response.status_code == 429 or (
+            response.status_code == 403 and url.startswith(KNOWN_403_ISSUES)
+        ):
             if verbose:
-                print("    ->", response.status_code)
+                print(
+                    ARROW,
+                    "Too many requests (429) or known forbidden (403) page, allowing exception.",
+                )
 
-            if response.status_code == 429 or (
-                response.status_code == 403 and url.startswith(KNOWN_403_ISSUES)
-            ):
-                continue
-            response.raise_for_status()
+        # Raise Exception corresponding to HTTP status
+        response.raise_for_status()
 
-        except requests.exceptions.SSLError as exc:
-            if verbose:
-                print("    ->", "SSLError")
-
-            if not url.startswith(KNOWN_SSL_ISSUES):
-                exceptions[url] = exc
-            else:
-                if verbose:
-                    print("    ->", "Known SSL issue, allowing exception.")
-
-        except Exception as exc:
-            exceptions[url] = exc
-
-    if exceptions:
-        raise RuntimeError(
-            "\n\n".join(
-                [f"Invalid URLs in {path=!s}"]
-                + [f"{url=}\n{exc!s}" for url, exc in exceptions.items()]
-            )
-        )
-    else:  # No exceptions
+    except requests.exceptions.SSLError as exc:
+        result = exc
         if verbose:
-            print("--- No errors found ✓ ---")
+            print(ARROW, "SSLError")
+
+        if url.startswith(KNOWN_SSL_ISSUES):
+            result = None
+            if verbose:
+                print(ARROW, "Known SSL issue, allowing exception.")
+
+    except Exception as exc:
+        result = exc
+        if verbose:
+            print(ARROW, exc)
+
+    else:
+        result = None
+
+    finally:
+        return result
 
 
 def main(paths: list[Path], verbose: bool = False) -> None:
-    for path in paths:
+    # Setup
+    if verbose:
+        print("Testing notebooks:")
+        print("\n".join(f"    {path}" for path in paths))
+
+    # Find all unique URLs in all notebooks
+    urls_by_notebook = {
+        path: find_urls(nbformat.read(path, nbformat.NO_CONVERT)) for path in paths
+    }
+    unique_urls = sorted(
+        {url for path, url_list in urls_by_notebook.items() for url in url_list}
+    )
+    notebooks_by_url = {
+        url: sorted(
+            path for path, url_list in urls_by_notebook.items() if url in url_list
+        )
+        for url in unique_urls
+    }
+
+    # Check each unique URL
+    url_checks = {url: validate_url(url, verbose=verbose) for url in unique_urls}
+
+    # Output: Raise error describing failing URLs
+    url_has_errors = {
+        url: check for url, check in url_checks.items() if check is not None
+    }
+
+    if url_has_errors:
+        raise RuntimeError(
+            "\n\n"
+            + "\n\n".join(
+                "\n".join(
+                    [f"Failing URL: {url}", "  Occurring in notebooks:"]
+                    + [f"    {path}" for path in notebooks_by_url[url]]
+                    + ["  Error:", f"    {exc!s}"]
+                )
+                for url, exc in url_has_errors.items()
+            )
+        ) from None
+    else:  # No exceptions
         if verbose:
-            print("\n---", path, "---")
-        validate_urls(path, verbose=verbose)
+            print("--- All URLs pass ✓ ---")
 
 
 if __name__ == "__main__":
